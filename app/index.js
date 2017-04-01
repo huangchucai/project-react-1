2956e5b9fa593df431b80710d3328424fef19411
huangchucai <425205839@qq.com>
2017-04-01 16:36:13 +0800

    引入自定义脚本

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash'
 import $ from 'jquery'
+import foo from './foo.js'
 function component(){
     var element=$('<div></div>')
     element.html(_.join(['Hello','webpack'],''));
     return element.get(0)
 }
 document.body.appendChild(component())
+console.log(foo)
+console.log(foo())
